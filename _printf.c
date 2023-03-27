8d2279c754e3a5174ebddcc86b02abee05dc99c3
mahmoud-elbelasy <hoodabelasy@gmail.com>
2023-03-27 06:36:25 -0700

    the beginnig

---
+++ _printf.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _printf - function that act like printf function
+ * @format: the object that will be print
+ * Return: ALways 0
+ */
+int _printf(char *format, ...)
+{
+	char *p;
+	unsigned int i;
+
+
+	va_list arg;
+	va_start(arg, format);
+
+	for (p = format; *p != '\0' ; p++)
+	{
+		i++;
+		while (*p != '%')
+		{
+			_putchar(*p);
+			p++;
+		}
+		p++;
+	}
+	va_end(arg);
+	return (i);
+}
+
---
+++ main.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(char *format, ...);
+
+int _putchar(char c);
+
